c2650cb0acf52754e7eaeca78055f64170e43828
mrdShinse <shinse1128@hotmail.com>
2017-04-15 20:11:46 +0900

    format elm code.

--- webpack/elm/Main.elm
+++ webpack/elm/Main.elm
@@ -1,6 +1,3 @@
--- Read all about this program in the official Elm guide:
--- https://guide.elm-lang.org/architecture/user_input/text_fields.html
-
 import Html exposing (Html, Attribute, beginnerProgram, text, div, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
@@ -8,7 +5,11 @@ import String
 
 
 main =
-  beginnerProgram { model = "", view = view, update = update }
+  beginnerProgram {
+    model = "",
+    view = view,
+    update = update
+  }
 
 
 -- UPDATE
@@ -18,20 +19,19 @@ type Msg = NewContent String
 update (NewContent content) oldContent =
   content
 
-
 -- VIEW
 
 view content =
-  div []
-    [ input [ placeholder "Text to reverse", onInput NewContent, myStyle ] []
-    , div [ myStyle ] [ text (String.reverse content) ]
-    ]
+  div [][
+    input [ placeholder "タグ検索", onInput NewContent, myStyle ] [],
+    div [ myStyle ] [ text (String.reverse content) ]
+  ]
 
 myStyle =
-  style
-    [ ("width", "100%")
-    , ("height", "40px")
-    , ("padding", "10px 0")
-    , ("font-size", "2em")
-    , ("text-align", "center")
-    ]
+  style [
+    ("width", "100%"),
+    ("height", "40px"),
+    ("padding", "10px 0"),
+    ("font-size", "2em"),
+    ("text-align", "center")
+  ]
