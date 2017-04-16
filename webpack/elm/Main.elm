253a18892e7a87f9041234b8c9a47fb74a33d51b
mrdShinse <shinse1128@hotmail.com>
2017-04-16 14:57:10 +0900

    create module searching-tag-textbox.

--- webpack/elm/Main.elm
+++ webpack/elm/Main.elm
@@ -1,37 +1,86 @@
-import Html exposing (Html, Attribute, beginnerProgram, text, div, input)
+import Html exposing (Html, Attribute, div, input, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-import String
+import Http
+import Json.Decode as Decode
 
 
+main : Program Never Model Msg
 main =
-  beginnerProgram {
-    model = "",
+  Html.program {
+    init = init,
     view = view,
-    update = update
+    update = update,
+    subscriptions = subscriptions
   }
 
+-- MODEL
+type alias Model = {
+  tags: List Tag
+}
+
+type alias Tag = {
+  id : String,
+  name : String
+}
+
+init : (Model, Cmd Msg)
+init = (Model [], Cmd.none)
 
 -- UPDATE
+type Msg
+  = Search String
+  | UpdateByHttpResponse (Result Http.Error (List Tag))
 
-type Msg = NewContent String
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg _ =
+  case msg of
+    Search (_)->
+      (Model [], fetchTagdata)
 
-update (NewContent content) oldContent =
-  content
+    UpdateByHttpResponse (Ok tags) ->
+      (Model tags, Cmd.none)
+    UpdateByHttpResponse (Err _) ->
+      (Model [], Cmd.none)
 
 -- VIEW
 
-view content =
-  div [][
-    input [ placeholder "タグ検索", onInput NewContent, myStyle ] [],
-    div [ myStyle ] [ text (String.reverse content) ]
+view : Model -> Html Msg
+view _ =
+  div [] [
+    input [ placeholder "タグ検索", onInput Search, myStyle ] [],
+    div [ myStyle ] []
   ]
 
+myStyle : Attribute msg
 myStyle =
-  style [
+  style[
     ("width", "100%"),
     ("height", "40px"),
     ("padding", "10px 0"),
     ("font-size", "2em"),
     ("text-align", "center")
   ]
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- HTTP
+fetchTagdata : Cmd Msg
+fetchTagdata =
+  let
+    url = "http://localhost:5000/tags.json"
+  in
+    Http.send UpdateByHttpResponse(Http.get url decodeTaglist)
+
+decodeTaglist : Decode.Decoder (List Tag)
+decodeTaglist =
+  Decode.list decodeTagdata
+
+decodeTagdata : Decode.Decoder Tag
+decodeTagdata =
+  Decode.map2 Tag
+    (Decode.field "id" Decode.string)
+    (Decode.field "name" Decode.string)
